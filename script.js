f4e28011fdd024873afb62e9650ca47e86480816
ona07 <b1022096@fun.ac.jp>
2025-11-26 12:53:35 +0900

    add url

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 ﻿// フロントエンド側設定：GASのデプロイURLを差し替えて使用する
-const GAS_BASE_URL = 'https://script.google.com/macros/s/DEPLOY_ID/exec'; // 例: https://script.google.com/macros/s/XXXX/exec
+const GAS_BASE_URL = 'https://script.googleusercontent.com/a/macros/fun.ac.jp/echo?user_content_key=AehSKLjgUGecf6rCf8cdtn1YrNTNfn7_6qUGDvdFLN1ANBig-KUTtvF7JfirTXWP9P1MH5YtIHWHpwZfxHqyyUdXos692N25NTsNiTUVpIQ9CeI_X2LlejvUAg-fpmOZpzYK_wa9MocEQ6qZ9c1veuh5uG8B9quasIhckt17E8txI3FBvgd720WNWBL16sNJFaaIj9XoJ52WKc3yHxZgLaDKsBHRDSwlvZyZm1gxzIC5n8UKuvD_tChQnTW4cKhBj-HvFR6Vt5K-qRu-6Nt4zvCD4i9xejW0ybnp_jUjO-kAgwB5mE85YaE&lib=MXSav2gLrMDMv2bFCIVzo6-TirzdffZ9W'; // 例: https://script.google.com/macros/s/XXXX/exec
 
 const statusText = document.getElementById('statusText');
 const commentEl = document.getElementById('comment');
